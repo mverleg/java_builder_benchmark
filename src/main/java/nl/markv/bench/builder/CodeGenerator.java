9fb4500dc0e8e5cb9b8da56f3e6ef6904e8bb68e
Mark <mverleg.noreply@gmail.com>
2022-07-21 21:30:33 +0200

    Initialize repository

---
+++ src/main/java/nl/markv/bench/builder/CodeGenerator.java
@@ -0,0 +1,7 @@
+package nl.markv.bench.builder;
+
+public class CodeGenerator {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
---
+++ src/test/java/nl/markv/bench/builder/CodeGeneratorTest.java
@@ -0,0 +1,12 @@
+package nl.markv.bench.builder;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class CodeGeneratorTest {
+	@Test
+	public void shouldAnswerWithTrue() {
+		assertTrue(true);
+	}
+}
