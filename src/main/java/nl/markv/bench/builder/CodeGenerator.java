e5d3c256d499e353206379e77f9be6d4635ade50
Mark <mverleg.noreply@gmail.com>
2022-07-21 21:51:19 +0200

    Little bit of code generator code

--- src/main/java/nl/markv/bench/builder/CodeGenerator.java
+++ src/main/java/nl/markv/bench/builder/CodeGenerator.java
@@ -1,7 +1,37 @@
 package nl.markv.bench.builder;
 
 public class CodeGenerator {
+
+	static class Type {
+		final String typeName;
+		final String exampleValue;
+		final String annotation;
+
+		public Type(String typeName, String exampleValue, String annotation) {
+			this.typeName = typeName;
+			this.exampleValue = exampleValue;
+			this.annotation = annotation;
+		}
+	}
+	static Type[] TYPES = new Type[]{
+			new Type("int", "2", "@Nonnull"),
+			new Type("Short", "1", "@Nonnull"),
+			new Type("String", "\"hello\"", "@Nonnull"),
+			new Type("String", null, "@Nullable"),
+			new Type("double", "3.14e0", "@Nonnull"),
+			new Type("Long", "Long.MAX_VALUE", "@Nullable"),
+			new Type("CharSequence", "null", "@Nullable"),
+	};
+
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+	}
+
+	void generateDataClass(String className, int fieldCount, int seed) {
+		var src = new StringBuilder();
+	}
+
+	void generateBuilder() {
+
 	}
 }
